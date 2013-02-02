20a87af5e9f2fe744724ad81543e2f22eb7dad3c
rei kubonaga <kubonagarei@gmail.com>
2013-02-03 04:58:22 +0900

    Add the passing point

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   def new
   end
   def create
-    datas = []
+    datas = { questions: [], passing_point: params[:passing_point] }
     params.keys.each do |key|
       next unless key.include?("question") && params[key].present?
       data = {}
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
       next if params["answers" + number].blank?
       data[:answers] = []
       params["answers" + number].each_with_index{ |answer, i| data[:answers].push({ answer: answer, point: params["points" + number][i] }) }
-      datas.push data
+      datas[:questions].push data
     end
     render json: datas
   end
