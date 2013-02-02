b685c451fac0356a0dff08aa8bee686ff041a045
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-02-03 03:25:33 +0900

    Modify generated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
+  # Available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :database_authenticatable, :registerable,
+  # :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :name, :github_id, :github_name, :github_token, :image_url
 end
