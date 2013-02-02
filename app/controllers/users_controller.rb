4fa4e2f5a533b04b04a3f0a4f1fbb5cc9de1339b
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-02-03 04:40:34 +0900

    Add users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+
+    redirect_to new_question_path
+  end
+end
