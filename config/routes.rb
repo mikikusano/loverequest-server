04b3eec99e3ce85b767b59900ae5014d1e14e937
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-02-03 04:51:03 +0900

    Annotate routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,3 +11,15 @@ LoveRequest::Application.routes.draw do
 end
 
 
+#== Route Map
+# Generated on 03 Feb 2013 04:50
+#
+# user_omniauth_authorize      /users/auth/:provider(.:format)        devise/omniauth_callbacks#passthru {:provider=>/github/}
+#  user_omniauth_callback      /users/auth/:action/callback(.:format) devise/omniauth_callbacks#(?-mix:github)
+#                   users GET  /users(.:format)                       users#index
+#                         POST /users(.:format)                       users#create
+#                new_user GET  /users/new(.:format)                   users#new
+#                    user GET  /users/:id(.:format)                   users#show
+#         travis_callback      /travis/callback(.:format)             travis#callback
+#               questions POST /questions(.:format)                   questions#create
+#            new_question GET  /questions/new(.:format)               questions#new
