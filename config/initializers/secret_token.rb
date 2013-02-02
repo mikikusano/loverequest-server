6964670571ce945b8456a40a40ce5f75125cb25a
rei kubonaga <kubonagarei@gmail.com>
2013-02-02 14:20:44 +0900

    rails new love-request --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoveRequest::Application.config.secret_token = 'b6e2e69580bb76cf9b49adac021a9f41f9e171d1f3f05038f207fed32b2704723df8aee4dec2800f4d6f4f05a47dffd3afae3604a298253fdbf72d5e60f928e4'
